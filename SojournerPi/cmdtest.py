48914fdce1b6b7a435f144d7e4ab8241e4e9ea9b
markwatneyy <samanthabelnavis@gmail.com>
2016-12-22 18:16:26 -0500

    cmdtest.py tests

--- SojournerPi/cmdtest.py
+++ SojournerPi/cmdtest.py
@@ -8,7 +8,7 @@ port = 8888        # Arbitrary non-privileged port
 
 #Debug Data
 print("host: " + host)
-print("port: " + port)
+print("port: ",port)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
 s.listen(1)

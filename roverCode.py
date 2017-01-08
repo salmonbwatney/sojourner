979aa9b5167c48f0ad94b7a153a4aee250ffa4cd
markwatneyy <samanthabelnavis@gmail.com>
2017-01-07 23:14:21 -0500

    gui tests [ci skip]

---
+++ roverCode.py
@@ -0,0 +1,26 @@
+# import packages
+
+import io
+import socket
+import struct
+import time
+import picamera
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (640, 480)
+    camera.framerate = 24
+
+    server_socket = socket.socket()
+    server_socket.bind(('0.0.0.0', 8000))
+    server_socket.listen(0)
+
+    #Accept single connection and make file-like object
+    connection = server_socket.accept()[0].makefile('wb')
+    try:
+        camera.start_recording(connection, format = 'h264')
+        camera.wait_recording(10)
+        camera.stop_recording()
+
+    finally:
+        connection.close()
+        server_socket.close()
